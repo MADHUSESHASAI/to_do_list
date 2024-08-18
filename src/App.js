ca87f6803d7ffbf0712a74489023e3a30b523a90
MADHU SESHA SAI <saimadhu570242gmail.com>
2024-08-18 16:57:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useRef,useEffect} from 'react'
+import './App.css'
+import { MdDeleteOutline } from "react-icons/md";
+
 
 function App() {
+  
+  
+  let name=useRef('')
+  const [info, setArtists] = useState([]);
+
+
+  useEffect(()=>
+  {
+    console.log(new Date().toLocaleDateString())
+    name.current.focus();
+  }
+  ,[]) 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div id='heading'>
+      To Do List
     </div>
-  );
+    <div id='main'>
+      <div id='date'>Date:<span>{new Date().toLocaleDateString()}</span></div>
+      <div id='container'>
+        <div id='input'>
+        <input
+        ref={name}
+      
+      />
+      <button onClick={() => {name.current.value.length===0?setArtists([...info]):
+        setArtists([...info,{value: name.current.value}])}}>Add</button>
+        </div>
+        <div id='box'>
+
+
+          {info.map((data,index)=>
+          <div id='items'>
+
+
+          <div id='name'>
+            {data.value}
+          </div>
+          
+          <div id='delete'>
+            <button onClick={()=>{info.splice(index,1)
+    setArtists([...info])}}>
+          <MdDeleteOutline />
+          </button>
+          </div>
+
+          </div>
+        )}
+          
+
+
+          
+        </div>
+      </div>
+    </div>
+    </>
+  )
+
 }
 
-export default App;
+export default App
